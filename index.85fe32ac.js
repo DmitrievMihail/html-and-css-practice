1fd824e235489240f7e46e005140626ebd9b48a9
Mihail Dmitriev <mmxdmv@mail.ru>
2022-12-21 01:25:03 +0400

    Updates

---
+++ index.85fe32ac.js
@@ -0,0 +1,2 @@
+const grid=document.createElement("div");grid.className="grid",document.body.appendChild(grid),document.addEventListener("keydown",(e=>{e.ctrlKey&&"KeyG"===e.code&&grid.classList.toggle("grid_visible")}));
+//# sourceMappingURL=index.85fe32ac.js.map
